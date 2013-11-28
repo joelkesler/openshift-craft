5f78a100a7d73ed5ce73125b4d733e87e694f613
Joel Kesler <joelkesler@gmail.com>
2013-11-27 20:28:05 -0500

    Retry

---
+++ .openshift/craft/app/Info.php
@@ -0,0 +1,20 @@
+<?php
+namespace Craft;
+
+/**
+ * Craft by Pixel & Tonic
+ *
+ * @package   Craft
+ * @author    Pixel & Tonic, Inc.
+ * @copyright Copyright (c) 2013, Pixel & Tonic, Inc.
+ * @license   http://buildwithcraft.com/license Craft License Agreement
+ * @link      http://buildwithcraft.com
+ */
+
+define('CRAFT_VERSION', '1.3');
+define('CRAFT_BUILD', '2422');
+define('CRAFT_SCHEMA_VERSION', '1.3.10');
+define('CRAFT_RELEASE_DATE', '1384523081');
+define('CRAFT_MIN_BUILD_REQUIRED', '2396');
+define('CRAFT_MIN_BUILD_URL', 'http://download.buildwithcraft.com/craft/1.2/1.2.2396/Craft-1.2.2396.zip');
+define('CRAFT_TRACK', 'stable');
